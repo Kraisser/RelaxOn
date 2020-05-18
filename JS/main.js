bab9f62d71be845f752482e785f00dc672cb92e2
Gaiko.Vladislav <vgaiko@mail.ru>
2020-05-18 14:30:02 +0300

    Adaptive menu fix 1.4

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
 document.getElementById(`hamburgerIconContainer`).addEventListener(`click`, function (e) {
 	e.currentTarget.classList.toggle(`open`);
 	document.getElementById(`hamburgerNavMenu`).classList.toggle(`hideMenu`);
+	document.getElementById(`hamburgerNavUl`).classList.toggle(`hideMenu`);
 });
