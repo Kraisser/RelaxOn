f83df2057790434ab51525d5e43233e9f16d0e2b
Gaiko.Vladislav <vgaiko@mail.ru>
2020-06-01 00:24:32 +0300

    2.0

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,31 @@
 document.getElementById(`hamburgerIconContainer`).addEventListener(`click`, function (e) {
 	e.currentTarget.classList.toggle(`open`);
 	document.getElementById(`hamburgerNavMenu`).classList.toggle(`hideMenu`);
-	document.getElementById(`hamburgerNavUl`).classList.toggle(`hideMenu`);
 });
 
+
+let scrollTimerId;
+
 window.addEventListener(`scroll`, function() {
-	if (pageYOffset >= 60) {
-		document.getElementById(`stocks`).style.display = `none`;
-	} else {
-		document.getElementById(`stocks`).style.display = `flex`;
+	clearTimeout(scrollTimerId);
+	if (document.body.clientWidth <= 700) {
+		scrollTimerId = setTimeout(stocksHide, 50);
 	}
 });
+
+function stocksHide() {
+	if (pageYOffset >= 60) {
+		document.getElementById(`stocks`).style.height = `0px`;		
+		hamburgerTopAdopt(100);
+	} else {
+		document.getElementById(`stocks`).style.height = `60px`;
+		hamburgerTopAdopt(160);
+	}
+	console.log(123);
+}
+
+function hamburgerTopAdopt(headerHeight) {
+	document.getElementById(`hamburgerNavMenu`).style.top = `${headerHeight}px`;
+}
 
 sliderModule.sliderLogicInit(2); // Интервал в секундах
