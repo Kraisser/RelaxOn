93fa3c8489b327faef873c3c86bf21bdb5fc76da
Gaiko.Vladislav <vgaiko@mail.ru>
2020-06-01 15:56:08 +0300

    Stocks fix 2.1.2

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,8 @@ document.getElementById(`hamburgerIconContainer`).addEventListener(`click`, func
 });
 
 
-let scrollTimerId;
+let scrollTimerId,
+		stocksHideFlag = false;
 
 window.addEventListener(`scroll`, function() {
 	clearTimeout(scrollTimerId);
@@ -13,18 +14,32 @@ window.addEventListener(`scroll`, function() {
 	}
 });
 
+window.addEventListener(`resize`, function() {
+	if (document.body.clientWidth >= 700) {
+		stocksShow();
+	}
+});
+
+function stocksShow() {
+	if (stocksHideFlag) {
+		document.getElementById(`stocks`).style.height = `60px`;
+		hamburgerTopAdopt(160);
+		stocksHideFlag = false;
+	}
+}
+
 function stocksHide() {
 	if (pageYOffset >= 60) {
 		document.getElementById(`stocks`).style.height = `0px`;		
 		hamburgerTopAdopt(100);
+		stocksHideFlag = true;
 	} else {
-		document.getElementById(`stocks`).style.height = `60px`;
-		hamburgerTopAdopt(160);
+		stocksShow();
 	}
 }
 
-function hamburgerTopAdopt(headerHeight) {
-	document.getElementById(`hamburgerNavMenu`).style.top = `${headerHeight}px`;
+function hamburgerTopAdopt(hamburgerTop) {
+	document.getElementById(`hamburgerNavMenu`).style.top = `${hamburgerTop}px`;
 }
 
 sliderModule.sliderLogicInit(2); // Интервал в секундах
